7af4f13d15714974f376d6d5779a36712d3c1c0b
Charles Marshall <charles@oneblackbear.com>
2013-02-06 15:25:12 +0000

    ini & setup

---
+++ setup.php
@@ -0,0 +1,10 @@
+<?
+CMSApplication::register_module("media", array("display_name"=>"Content", "link"=>"/admin/content/", 'split'=>true));
+
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
+AutoLoader::$plugin_array[] = array("name"=>"wildfire.content","dir"=>__DIR__);
+
+
+?>
