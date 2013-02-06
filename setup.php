0d02821000403b7515e4f8bffb39f4005e0c74da
Charles Marshall <charles@oneblackbear.com>
2013-02-06 15:58:23 +0000

    wrong name

--- setup.php
+++ setup.php
@@ -1,5 +1,5 @@
 <?
-CMSApplication::register_module("media", array("display_name"=>"Content", "link"=>"/admin/content/", 'split'=>true));
+CMSApplication::register_module("content", array("display_name"=>"Content", "link"=>"/admin/content/", 'split'=>true));
 
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
