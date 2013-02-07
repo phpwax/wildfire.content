d9409443baeda0f8eb273428252998f1b6aa21db
Charles Marshall <charles@oneblackbear.com>
2013-02-07 10:01:26 +0000

    offload that call to the registered helper, this still needs a bit of work

--- lib/model/WildfireContent.php
+++ lib/model/WildfireContent.php
@@ -285,31 +285,20 @@ class WildfireContent extends WaxTreeModel {
 
    //this will need updating when the framework can handle manipulating join columns
   public function file_meta_set($fileid, $tag, $order=0, $title=''){
-    // $model = new WaxModel;
-    // if($this->table < "wildfire_media") $model->table = $this->table."_wildfire_media";
-    // else $model->table = "wildfire_media_".$this->table;
 
-    // $col = $this->table."_".$this->primary_key;
-    // if(!$order) $order = 0;
-    // if(($found = $model->filter($col, $this->primval)->filter("wildfire_media_id", $fileid)->all()) && $found->count()){
-    //   foreach($found as $r){
-    //     $sql = "UPDATE `".$model->table."` SET `join_order`=$order, `tag`='$tag', `title`='$title' WHERE `id`=$r->primval";
-    //     $model->query($sql);
-    //   }
-    // }else{
-    //   $sql = "INSERT INTO `".$model->table."` (`wildfire_media_id`, `$col`, `join_order`, `tag`, `title`) VALUES ('$fileid', '$this->primval', '$order', '$tag', '$title')";
-    //   $model->query($sql);
-    // }
+    foreach(CMSApplication::$handlers['media'] as $handler){
+      $class = new $handler;
+      $class->file_meta_set($this->primval, $fileid, $tag, $order, $title, $this->table, $this->primary_key);
+    }
   }
   public function file_meta_get($fileid=false, $tag=false){
-    // $model = new WaxModel;
-    // if($this->table < "wildfire_media") $model->table = $this->table."_wildfire_media";
-    // else $model->table = "wildfire_media_".$this->table;
-    // $col = $this->table."_".$this->primary_key;
-    // if($fileid) return $model->filter($col, $this->primval)->filter("wildfire_media_id", $fileid)->order('join_order ASC')->first();
-    // elseif($tag=="all") return $model->filter($col, $this->primval)->order('join_order ASC')->all();
-    // elseif($tag) return $model->filter($col, $this->primval)->filter("tag", $tag)->order('join_order ASC')->all();
-    // else return false;
+    $return = false;
+    foreach(CMSApplication::$handlers['media'] as $handler){
+      $class = new $handler;
+      $return = $class->file_meta_get($this->primval, $fileid, $tag, $this->table, $this->primary_key);
+    }
+    return $return;
+
   }
 
   public function format_content() {
