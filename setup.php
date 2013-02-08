cb1afa01fb7ffc726b81b185db6327eb9042c9b2
Charles Marshall <charles@oneblackbear.com>
2013-02-08 15:56:52 +0000

    trying the new assets

--- setup.php
+++ setup.php
@@ -1,11 +1,12 @@
 <?
-CMSApplication::register_module("content", array("display_name"=>"Content", "link"=>"/admin/content/", 'split'=>true, 'plugin_name'=>'wildfire-content', 'assets_for_cms'=>true));
+CMSApplication::register_module("content", array("display_name"=>"Content", "link"=>"/admin/content/", 'split'=>true));
+CMSApplication::register_asset("wildfire", "js", "wildfire.content");
 
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
 AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
 
-AutoLoader::register_assets("javascripts/wildfire-content",__DIR__."/resources/public/javascripts/wildfire-content/", "/*.js");
+AutoLoader::register_assets("javascripts/wildfire.content",__DIR__."/resources/public/javascripts/wildfire.content/", "/*.js");
 
 AutoLoader::$plugin_array[] = array("name"=>"wildfire.content","dir"=>__DIR__);
 
