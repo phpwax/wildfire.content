ef1f6dd24a83d0e21e4685cfda1ef0dd13d91851
Charles Marshall <charles@oneblackbear.com>
2013-02-08 16:18:21 +0000

    autoloader stuff needs to be in ini, cms in setup

--- ini.php
+++ ini.php
@@ -1,3 +1,9 @@
 <?
+AutoLoader::register_assets("javascripts/wildfire-content", __DIR__."/resources/public/javascripts/wildfire-content/", "/*.js");
+AutoLoader::register_view_path("plugin", __DIR__."/view/");
+AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
+AutoLoader::$plugin_array[] = array("name"=>"wildfire.content","dir"=>__DIR__);
+
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
 ?>
--- setup.php
+++ setup.php
@@ -1,14 +1,5 @@
 <?
 CMSApplication::register_module("content", array("display_name"=>"Content", "link"=>"/admin/content/", 'split'=>true));
-CMSApplication::register_asset("wildfire", "js", "wildfire.content");
-
-AutoLoader::register_view_path("plugin", __DIR__."/view/");
-AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
-AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
-
-AutoLoader::register_assets("javascripts/wildfire.content",__DIR__."/resources/public/javascripts/wildfire.content/", "/*.js");
-
-AutoLoader::$plugin_array[] = array("name"=>"wildfire.content","dir"=>__DIR__);
-
+CMSApplication::register_asset("wildfire", "js", "wildfire-content");
 
 ?>
