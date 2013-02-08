124e456c57908ca9a04f0c5634c976f9394c8e44
Charles Marshall <charles@oneblackbear.com>
2013-02-08 18:39:52 +0000

    renamed

--- setup.php
+++ setup.php
@@ -1,5 +1,5 @@
 <?
 CMSApplication::register_module("content", array("display_name"=>"Content", "link"=>"/admin/content/", 'split'=>true));
-CMSApplication::register_asset("wildfire", "js", "wildfire.content");
+CMSApplication::register_asset("wildfire", "js", "wildfire-content");
 
 ?>
