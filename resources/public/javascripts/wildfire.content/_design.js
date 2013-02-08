9f76f75c47ab1a4c52859e33bc177eeacd8da479
Charles Marshall <charles@oneblackbear.com>
2013-02-08 16:35:18 +0000

    iframe src change in the bound event

--- resources/public/javascripts/wildfire.content/_design.js
+++ resources/public/javascripts/wildfire.content/_design.js
@@ -1,8 +1,12 @@
 jQuery(document).ready(function(){
 
+  jQuery(window).bind("autosave.completed", function(e, res){
+    jQuery(".design_preview iframe").attr("src", jQuery(".preview-button").attr("href"));
+  });
+
   jQuery(".field-design-pagetype select, .field-design-layout select").bind("change", function(){
     auto_save_form(jQuery('#auto-save span'), jQuery('#auto-save'), jQuery('#auto-save img'));
-    jQuery(".design_preview iframe").attr("src", jQuery(".preview-button").attr("href"));
+
   });
 
 });
