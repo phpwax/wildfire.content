7278b71c1fbb75b791cfa1637a6ab940e329eb6f
Charles Marshall <charles@oneblackbear.com>
2013-02-08 14:53:32 +0000

    include js

--- setup.php
+++ setup.php
@@ -4,6 +4,9 @@ CMSApplication::register_module("content", array("display_name"=>"Content", "lin
 AutoLoader::register_view_path("plugin", __DIR__."/view/");
 AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
 AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
+
+AutoLoader::register_assets("javascripts/wildfire.content",__DIR__."/resources/public/javascripts/wildfire.content/", "/*.js");
+
 AutoLoader::$plugin_array[] = array("name"=>"wildfire.content","dir"=>__DIR__);
 
 
