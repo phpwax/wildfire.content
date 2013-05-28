6c0a4052f94898cb0242e37c1efecf1af634da1b
mikenosko <michal@oneblackbear.com>
2013-05-28 13:59:36 +0100

    autosave form function call change id on save button to work with function

--- resources/public/javascripts/wildfire-content/_design.js
+++ resources/public/javascripts/wildfire-content/_design.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function(){
   });
 
   jQuery(".field-design-pagetype select, .field-design-layout select").bind("change", function(){
-    auto_save_form(jQuery('#auto-save span'), jQuery('#auto-save'), jQuery('#auto-save img'));
+    auto_save_form(jQuery('#auto-save'));
 
   });
 
