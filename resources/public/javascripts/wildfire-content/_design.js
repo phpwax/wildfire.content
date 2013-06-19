c6e2b55822629fc100889557109f4d7a1a1fe204
Charles Marshall <charles@oneblackbear.com>
2013-06-19 13:11:48 +0100

    add a timer to delay this change

--- resources/public/javascripts/wildfire-content/_design.js
+++ resources/public/javascripts/wildfire-content/_design.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function(){
 
   jQuery(window).bind("autosave.completed", function(e, res){
-    jQuery(".design_preview iframe").attr("src",res.model.row.permalink+"?preview="+res.meta.model.primval).get(0).contentWindow.location.reload();
+    setTimeout(function(){
+
+      jQuery(".design_preview iframe").attr("src",res.model.row.permalink+"?preview="+res.meta.model.primval).get(0).contentWindow.location.reload();
+    }, 600);
   });
 
   jQuery(".field-design-pagetype select, .field-design-layout select").bind("change", function(){
