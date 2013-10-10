c7234881928b7191bae61158e8e2118ef2fbf055
Ross Riley <riley.ross@gmail.com>
2013-10-10 12:23:58 +0100

    Change the order to process embeds before inline

--- lib/filters/CmsTextFilter.php
+++ lib/filters/CmsTextFilter.php
@@ -9,7 +9,7 @@ class CmsTextFilter  {
   
   
   static public $filters = array(
-    "before_save"=>array("convert_chars", "strip_attributes", "strip_slashes", "inline_images","embedded_images"),
+    "before_save"=>array("convert_chars", "strip_attributes", "strip_slashes", "embedded_images","inline_images"),
     "before_output"=> array("strip_slashes","empty_paragraphs", "first_para_hook")
   );
   
