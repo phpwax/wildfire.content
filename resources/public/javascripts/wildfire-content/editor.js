4e42e2ad7f26c223733a49872cfae8cbf01412cc
Ross Riley <riley.ross@gmail.com>
2013-11-03 13:42:04 +0000

    add some debugs to test functionality

--- resources/public/javascripts/wildfire-content/editor.js
+++ resources/public/javascripts/wildfire-content/editor.js
@@ -3,15 +3,15 @@ var editorPositionBookmark;
 jQuery(document).ready(function($) {
 
 	jQuery(window).bind("autosave.start", function(e, res){
-		editorPositionBookmark = tinyMCE.activeEditor.selection.getBookmark();
-		console.log(editorPositionBookmark);
+		res.execCommand('insertHTML',false, '<span id="caret">&nbsp;</span>');
 	});
 
 	jQuery(window).bind("autosave.completed", function(e, res){
 		var editor = tinyMCE.activeEditor;
 		editor.setContent(res.model.row.content);
-		console.log(editorPositionBookmark);
-		
+		var caret = ed.dom.select('span#caret');
+		console.log(caret);
+
 	});
 
 });
