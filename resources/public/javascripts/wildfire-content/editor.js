2b688db16f5c443fe0d6d88ab4fa4699f7d19a94
Ross Riley <riley.ross@gmail.com>
2013-11-03 13:10:52 +0000

    new strategy to maintain position of caret

--- resources/public/javascripts/wildfire-content/editor.js
+++ resources/public/javascripts/wildfire-content/editor.js
@@ -3,15 +3,14 @@ jQuery(document).ready(function($) {
 
 	jQuery(window).bind("autosave.start", function(e, res){
 		var endId = tinymce.DOM.uniqueId();
-		res.dom.add(res.getBody(), 'span', {'id': endId}, '');
+		res.dom.add(res.getBody(), 'div', {'id': endId}, '');
 	});
 
 	jQuery(window).bind("autosave.completed", function(e, res){
-		console.log(auto_save_bookmark);
 		var editor = tinyMCE.activeEditor;
 		editor.setContent(res.model.row.content);
 
-		var newNode = editor.dom.select('span#' + endId);
+		var newNode = editor.dom.select('div#' + endId);
 		editor.selection.select(newNode[0]);
 	});
 
