8000ea2cc4a5c1ce825de8070c7befb8c4cae076
Ross Riley <riley.ross@gmail.com>
2013-11-03 12:29:22 +0000

    attempt to preserve bookmark between content updates

--- resources/public/javascripts/wildfire-content/editor.js
+++ resources/public/javascripts/wildfire-content/editor.js
@@ -1,10 +1,14 @@
+var auto_save_bookmark;
 jQuery(document).ready(function($) {
+
+	jQuery(window).bind("autosave.start", function(e, res){
+		auto_save_bookmark = res.getBookmark();
+	});
 
 	jQuery(window).bind("autosave.completed", function(e, res){
 		var editor = tinyMCE.activeEditor;
-		var bookmark = editor.selection.getBookmark(2, true);
 		editor.setContent(res.model.row.content);
-		editor.selection.moveToBookmark(bookmark);
+		editor.selection.moveToBookmark(auto_save_bookmark);
 	});
 
 });
