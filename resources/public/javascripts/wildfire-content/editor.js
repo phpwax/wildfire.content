2a68e3767c544406efce378b183a8cb5e0993072
Ross Riley <riley.ross@gmail.com>
2013-11-03 14:46:16 +0000

    fix to bookmark

--- resources/public/javascripts/wildfire-content/editor.js
+++ resources/public/javascripts/wildfire-content/editor.js
@@ -3,15 +3,13 @@ var editorPositionBookmark;
 jQuery(document).ready(function($) {
 
 	jQuery(window).bind("autosave.start", function(e, res){
-		alert("here");
-		res.execCommand('insertHTML',false, '<span id="caret">&nbsp;</span>');
+		editorPositionBookmark = res.selection.getBookmark();
 	});
 
 	jQuery(window).bind("autosave.completed", function(e, res){
 		var editor = tinyMCE.activeEditor;
 		editor.setContent(res.model.row.content);
-		var caret = editor.dom.select('span#caret');
-		
+		editor.selection.moveToBookmark(editorPositionBookmark);
 
 	});
 
