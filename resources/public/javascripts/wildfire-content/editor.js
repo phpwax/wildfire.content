7ee727fd7e5e8b60210cd9a5b498aab160afc816
Ross Riley <riley.ross@gmail.com>
2013-11-03 13:55:18 +0000

    fix to variable

--- resources/public/javascripts/wildfire-content/editor.js
+++ resources/public/javascripts/wildfire-content/editor.js
@@ -3,6 +3,7 @@ var editorPositionBookmark;
 jQuery(document).ready(function($) {
 
 	jQuery(window).bind("autosave.start", function(e, res){
+		alert("here");
 		res.execCommand('insertHTML',false, '<span id="caret">&nbsp;</span>');
 	});
 
@@ -10,7 +11,7 @@ jQuery(document).ready(function($) {
 		var editor = tinyMCE.activeEditor;
 		editor.setContent(res.model.row.content);
 		var caret = editor.dom.select('span#caret');
-		console.log(caret);
+		
 
 	});
 
