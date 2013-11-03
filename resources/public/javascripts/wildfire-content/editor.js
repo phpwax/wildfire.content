faab298849bcf654417e5edb09f6d2f38b697f40
Ross Riley <riley.ross@gmail.com>
2013-11-03 21:59:30 +0000

    fix

--- resources/public/javascripts/wildfire-content/editor.js
+++ resources/public/javascripts/wildfire-content/editor.js
@@ -3,14 +3,14 @@ var editorPositionBookmark;
 jQuery(document).ready(function($) {
 
 	jQuery(window).bind("autosave.start", function(e, res){
-		editorPositionBookmark = res.selection.getBookmark();
+		editorPositionBookmark = res.selection.getBookmark(2,true);
 	});
 
 	jQuery(window).bind("autosave.completed", function(e, res){
 		var editor = tinyMCE.activeEditor;
 		editor.setContent(res.model.row.content);
 		editor.selection.moveToBookmark(editorPositionBookmark);
-    editor.execCommand('insertText', false, "");
+    editor.execCommand('InsertText', false, " ");
 	});
 
 });
