2d4a87a26cd8d518aa83a8fbb2ef953b6182d64a
Ross Riley <riley.ross@gmail.com>
2013-11-04 21:52:44 +0000

    i think this fix should do the job

--- resources/public/javascripts/wildfire-content/editor.js
+++ resources/public/javascripts/wildfire-content/editor.js
@@ -1,15 +1,20 @@
 var editorPositionBookmark;
+var editorScrollTop;
 
 jQuery(document).ready(function($) {
 
 	jQuery(window).bind("autosave.start", function(e, res){
+		editorScrollTop = $(res.getDoc()).scrollTop();
 		editorPositionBookmark = res.selection.getBookmark(2,true);
 	});
 
 	jQuery(window).bind("autosave.completed", function(e, res){
 		var editor = tinyMCE.activeEditor;
-		editor.setContent(res.model.row.content,{ no_events: true });
+		editor.setContent(res.model.row.content);
 		editor.selection.moveToBookmark(editorPositionBookmark);
+		setTimeout(function(){
+			$(editor.getDoc()).scrollTop(editorScrollTop);
+		}, 100);
 	});
 
 });
